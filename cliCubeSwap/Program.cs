8b10c157875cd0f9311ec90d4920a6ffb13930a7
Высоцкий Олег <Olegbom@gmail.com>
2025-09-05 16:01:44 +0300

    move braille renderer to another file

--- cliCubeSwap/Program.cs
+++ cliCubeSwap/Program.cs
@@ -3,6 +3,8 @@ using System.Text;
 using System.Diagnostics;
 using Microsoft.Extensions.Configuration;
 
+namespace CliCubeSwap;
+
 class Program
 {
     static CancellationTokenSource _cts = new CancellationTokenSource();
@@ -31,84 +33,4 @@ class Program
     }
 }
 
-public class BrailleFontRenderer
-{
-    public int Width { get; init; } = 128;
-    public int Height { get; init; } = 64;
 
-    public int Framerate { get; init; } = 60;
-
-    private byte[] _drawBuffer;
-
-    private string pattern = "⠀⢀⡠⠒⠉⠈⠑⠢⣀⠀" +
-                             "⡎⠁⠀⠀⠀⠀⠀⠀⠀⠉" + 
-                             "⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀" + 
-                             "⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀"; 
-
-    private const char BrailleEmptySymbol = '⠀';
-
-    public BrailleFontRenderer()
-    {
-        int bufferSizeIfBytes = Width * Height / 8;
-        _drawBuffer = new byte[bufferSizeIfBytes];
-        for (int row = 0; row < 4; row++)
-        {
-            int rowDelta = -(row % 2) * 5;
-            for (int column = 0; column < 7; column++)
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    int i_index = i + column * 10 + rowDelta;
-                    if (i_index >= 0 && i_index < Width / 2)
-                    {
-                        for (int j = 0; j < 4; j++)
-                        {
-                            int index = i_index + Width / 2 * (j + row * 4);
-                            _drawBuffer[index] = (byte)(pattern[i + 10 * j] - BrailleEmptySymbol);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    public async Task Loop(CancellationToken token)
-    {
-        await Task.Run( () => {
-            Console.CursorVisible = false;
-            StringBuilder sb = new StringBuilder();
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-
-            long frame = 0;
-
-            while (!token.IsCancellationRequested)
-            {
-                frame++;
-                for (int j = 0; j < Height/4; j++)
-                {
-                    for (int i = 0; i < Width/2; i++)
-                    {
-                        sb.Append((char)(BrailleEmptySymbol + _drawBuffer[i + Width/2*j]));
-                    }
-                    sb.AppendLine();
-                }
-                Console.Write(sb.ToString());
-                sb.Clear();
-                Console.Write("\e[16A");
-                while (frame * 1000.0 / Framerate > sw.ElapsedMilliseconds)
-                {
-                    Thread.Sleep(1);
-                }
-            }
-            Console.Write("\e[16B");
-        });
-         
-    }
-
-    private void Draw()
-    {
-        
-    }
-
-}
